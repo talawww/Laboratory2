ad7772b2e4d469428222fa83985d26ff5620e0f6
talawww <esaymatira@gmail.com>
2023-09-25 23:09:58 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/main', 'MusicController::index');
+$routes->get('/search', 'MusicController::searchsong');
+$routes->post('/addsong', 'MusicController::addsong');
+$routes->post('/createplaylist', 'MusicController::createplaylist');
+$routes->get('/deleteplaylist/(:num)', 'MusicController::deleteplaylist/$1');
+$routes->get('/selectedplaylist/(:any)', 'MusicController::selectedplaylist/$1');
+$routes->get('/addmusictoplaylist/(:num)', 'MusicController::addmusictoplaylist/$1');
+$routes->get('/removemusicfromplaylist/(:num)', 'MusicController::removemusicfromplaylist/$1');
